378ce793d48cc21b1660a99e2a33dc0c0a4a09f3
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-10 17:38:12 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,9 @@ variable "aws_account_role" {
 }
 
 variable "keybase_user" {}
+
+variable "iam_user_access_to_billing" {
+  default = true
+  type = "string"
+  description = "Allow IAM User Access to Billing"
+}
